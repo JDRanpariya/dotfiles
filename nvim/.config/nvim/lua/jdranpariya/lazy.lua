6bb5024fc43946ab2ba30fcef65b20d50698d4c0
JDRanpariya <jdpatel.code@gmail.com>
2026-03-20 11:59:42 +0100

    fix blinkcmp preset

--- nvim/.config/nvim/lua/jdranpariya/lazy.lua
+++ nvim/.config/nvim/lua/jdranpariya/lazy.lua
@@ -317,7 +317,7 @@ require("lazy").setup({
     version = "*",
     lazy = false,   -- must be available before LSP config runs
     opts = {
-        keymap = { preset = "default" },
+        keymap = { preset = "enter" },
         appearance = {
             use_nvim_cmp_as_default = false,
             nerd_font_variant = "mono",
